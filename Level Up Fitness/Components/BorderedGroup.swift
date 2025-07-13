06e130ff1c605f7a9734a2fdea6f29472938c93d
Jake Gray <graymountainmedia@gmail.com>
2025-07-13 11:02:32 -0600

    feat: implement onboarding flow with profile setup after email signup

---
+++ Level Up Fitness/Components/BorderedGroup.swift
@@ -0,0 +1,18 @@
+//
+//  BorderedGroup.swift
+//  Level Up Fitness
+//
+//  Created by Jake Gray on 7/13/25.
+//
+
+import SwiftUI
+
+struct BorderedGroup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BorderedGroup()
+}
